5360074b97623515cee9e931dc9aa47cab1496b9
Steve+Ari <steve@ariccademy.io>
2015-07-03 14:01:06 -0400

    Add follow/unfollow text.

--- ajax-twitter-skeleton/app/assets/javascripts/follow_toggle.js
+++ ajax-twitter-skeleton/app/assets/javascripts/follow_toggle.js
@@ -1,9 +1,20 @@
 $.FollowToggle = function (el) {
-  
+  this.$el = $(el);
+  this.$userId = $(this.$el.data("user-id"));
+  this.$followState = $(this.$el.data("initial-follow-state"));
+
+  this.$el.text(this.render());
 };
 
-$.FollowToggle.prototype.method1 = function () {
-// ...
+$.FollowToggle.prototype.render = function () {
+  var text;
+  if (this.$followState) {
+    text = "Unfollow!";
+  } else {
+    text = "Follow!"
+  };
+
+  return text;
 };
 
 $.fn.followToggle = function () {
